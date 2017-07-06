5dfec15c1a7ec0d780f957213af794129972a488
Cole Meurer <cole.meurer@gmail.com>
2017-07-06 11:33:48 +0900

    customized routes and added Doses controller

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -18,47 +18,34 @@ class CocktailsController < ApplicationController
   end
 
   # GET /cocktails/1/edit
-  def edit
-  end
+  # def edit
+  # end
 
   # POST /cocktails
   # POST /cocktails.json
   def create
     @cocktail = Cocktail.new(cocktail_params)
-
-    respond_to do |format|
-      if @cocktail.save
-        format.html { redirect_to @cocktail, notice: 'Cocktail was successfully created.' }
-        format.json { render :show, status: :created, location: @cocktail }
-      else
-        format.html { render :new }
-        format.json { render json: @cocktail.errors, status: :unprocessable_entity }
-      end
-    end
+    @cocktail.save ? redirect_to @cocktail : render :new
   end
 
   # PATCH/PUT /cocktails/1
   # PATCH/PUT /cocktails/1.json
-  def update
-    respond_to do |format|
-      if @cocktail.update(cocktail_params)
-        format.html { redirect_to @cocktail, notice: 'Cocktail was successfully updated.' }
-        format.json { render :show, status: :ok, location: @cocktail }
-      else
-        format.html { render :edit }
-        format.json { render json: @cocktail.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+    # respond_to do |format|
+    #   if @cocktail.update(cocktail_params)
+    #     format.html { redirect_to @cocktail, notice: 'Cocktail was successfully updated.' }
+    #     format.json { render :show, status: :ok, location: @cocktail }
+    #   else
+    #     format.html { render :edit }
+    #     format.json { render json: @cocktail.errors, status: :unprocessable_entity }
+    #   end
+    # end
+  # end
 
   # DELETE /cocktails/1
   # DELETE /cocktails/1.json
   def destroy
     @cocktail.destroy
-    respond_to do |format|
-      format.html { redirect_to cocktails_url, notice: 'Cocktail was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
