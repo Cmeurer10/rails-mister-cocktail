0748447742a90d8ebd53bc9453be34100206dbb4
Cole Meurer <cole.meurer@gmail.com>
2017-07-06 12:23:41 +0900

    Doses controller functionality added

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,7 +1,17 @@
 class DosesController < ApplicationController
   def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = @cocktail.doses.new(dose_params)
+    @cocktail.save ? redirect_to @cocktail : render :new
   end
 
   def destroy
+    Dose.find(params[:id]).destroy
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
